bdaa4a4c9e8d066776f1d4197b2cf9e2f4083126
CamiCamposs <camilacampossaldarriaga@gmail.com>
2024-03-29 12:57:43 -0300

    Correccion del link al cronograma

--- Materiales/Cronograma/ISW_Cronograma.bat
+++ Materiales/Cronograma/ISW_Cronograma.bat
@@ -1,2 +1,2 @@
 @echo off
-start https://docs.google.com/spreadsheets/d/1E3jGB3xii8EzUbWid9iNz1Z6MR68mfqV/edit?usp=sharing&ouid=102946796480727341645&rtpof=true&sd=true
+start https://docs.google.com/spreadsheets/d/13MJK0KhnCTZqRXcUbZSNNjE_p4cpQBzF/edit#gid=1986967239
